c58ba94cb9aebba6bccb4e128fff93dd31809aa9
sahilkubal <sahilkubal4@gmail.com>
2023-03-30 20:01:11 +0530

    firstcommit

---
+++ account/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from . models import User 
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    # We are writing this becoz we need confirm password field in
+    # our Registration Request
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only = True)
+    class Meta:
+        model = User 
+        fields = ['email', 'name','phone', 'password', 'password2', 'tc']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+        
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        
+        if password != password2:
+            raise serializers.ValidationError('Password and Confirm Password doesn\'t match')
+        return attrs
+    
+    def create(self, validate_data):
+        return User.objects.create_user(
+            **validate_data
+        )
+        
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    
+    email = serializers.EmailField(max_length = 255)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User 
+        fields = ['id', 'email', 'name','phone']
+        
+    
+class UserChangePasswordSerializer(serializers.Serializer):
+    
+    password = serializers.CharField(max_length = 255, style = {'input_type':'password'}, write_only = True)
+    password2 = serializers.CharField(max_length = 255, style = {'input_type':'password'}, write_only = True)
+    
+    class Meta:
+        fields = ['password', 'password2']
+        
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        user = self.context.get('user')
+        
+        if password != password2:
+            raise serializers.ValidationError('Password and confirm password doesn\'t match')
+        user.set_password(password)
+        user.save()
+        return attrs
+    
